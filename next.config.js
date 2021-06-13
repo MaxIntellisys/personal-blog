f126fa015c3a9de389e0149de95a0bef98cbf6c2
Max Sierra <msierra@intellisys.com.do>
2021-06-13 18:17:12 -0400

    Config for netlify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx$/
-  })
-  
-  module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
-  })
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+});
+
+module.exports = {
+  target: "serverless",
+};
