cd25bb09db767d57ccd2c83a5fa6be37e3aa1358
Max Sierra <msierra@intellisys.com.do>
2021-06-21 15:42:40 -0400

    Add airbnb style guide for eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,35 @@
 module.exports = {
-  settings: {
-    react: {
-      version: 'detect',
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
-  },
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        node: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    semi: ['error', 'always'],
-    quotes: ['error', 'single'],
-    'react/prop-types': 'off', // Let typescript handle this.
-    'react/react-in-jsx-scope': 'off', // With version 17 react does not have to be in scope.
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-base',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        // semi: ['error', 'always'],
+        // quotes: ['error', 'single'],
+        'react/prop-types': 'off', // Let typescript handle this.
+        'react/react-in-jsx-scope': 'off', // With version 17 react does not have to be in scope.
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
 };
